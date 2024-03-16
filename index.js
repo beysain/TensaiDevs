10a94062dcbf35cbb86240973b2d8fd56d3659e9
Bilal Shah <creativerajustbegun.com>
2024-03-16 20:38:43 +0500

    kata-5

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-console.log("Hello World!");
-arr = [1, 2, 3, 4, 5];
-console.log(arr[1]);
+// Write an algorithm that takes an array and moves all of the zeros to the end,
+// preserving the order of the other elements.
+
+function moveZeros(arr) {
+  let numberIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[numberIndex] = arr[i];
+      numberIndex++;
+    }
+  }
+  while (numberIndex < arr.length) {
+    arr[numberIndex] = 0;
+    numberIndex++;
+  }
+  return arr;
+}
+
+console.log(moveZeros([0, 1, 2, 3, 0, 0, 3, 4, 5]));
